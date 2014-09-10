e41a6971c50b4bada0e87acf5b7a5829133303ad
Ricardo Paiva <werneck.paiva@gmail.com>
2014-09-10 00:27:51 -0300

    Sample application location refactored

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,75 +1,25 @@
 module.exports = function(grunt) {
 
-    // Load grunt tasks automatically
-    require('load-grunt-tasks')(grunt);
-
     // Project configuration.
     grunt.initConfig({
         pkg : grunt.file.readJSON('package.json'),
-        config: {
-            dist: 'dist',
-            src: 'src',
-            test: 'spec'
+        config : {
+            destination : 'js/',
         },
-        concat : {
-            options : {
-                separator : ';'
-            },
-            dist : {
-                src : [ '<%= config.src %>/**/*.js' ],
-                dest : '<%= config.dist %>/js/<%= pkg.name %>.js'
-            }
-        },
-        uglify : { 
-            options : { 
-                banner : '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-            },  
-            dist : { 
-                files : { 
-                    '<%= config.dist %>/js/<%= pkg.name %>.min.js' : [ '<%= concat.dist.dest %>' ]
-                }   
-            },  
-        },
-        jshint: {
-            files: ['gruntfile.js', '<%= config.src %>/**/*.js', 'spec/**/*.js'],
-            options: {
-                globals: {
-                    jQuery: true,
-                    console: true,
-                    module: true
-              }   
-            }   
-        }, 
-        jasmine : {
-            retrato: {
-                src : '<%= config.src %>/**/*.js',
-                version: '2.0.0',
-                tasks: 'jasmine:pivotal:build',
-                options: {
-                    keepRunner: true,
-                    specs : 'spec/**/*Spec.js',
-                    helpers : 'spec/helpers/*.js',
-                    vendor: [
-                       'bower_components/jquery/dist/jquery.js',
-                       'bower_components/mustache.js/mustache.js',
-                       'bower_components/watch/src/watch.js',
-                    ]
+        bower : {
+            dev : {
+                dest : '<%= config.destination %>',
+                options : {
+                    stripAffix : false
                 }
             }
         },
-        watch: {
-            scripts: {
-                files: ['<%= config.src %>/**/*'],
-                tasks: ['default'],
-                options: {
-                    spawn: false,
-                },
-            },
-        },
-        clean: ['<%= config.dist %>']
+        clean : [ '<%= config.destination %>' ]
     });
 
+    grunt.loadNpmTasks('grunt-bower');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+
     // Default task(s).
-    grunt.registerTask('default', ['clean', 'concat', 'uglify']);
-    grunt.registerTask('test', ['jshint', 'jasmine']);
+    grunt.registerTask('default', [ 'clean', 'bower' ]);
 };
--- README.md
+++
@@ -1,6 +0,0 @@
-Retrato-JS
-==========
-
-Perfect balanced Photo Album javascript plugin
-
-You can use stand alone or with the retrato project.
